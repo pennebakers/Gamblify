1fa5303a4feffb0c5893a5d721bafed11cb5e21b
Seth Pennebaker <fusecreated@gmail.com>
2023-06-10 19:53:30 -0400

    APISCLT-0004: Adding a Basic Home page to App.js

--- gamblify-ui/src/App.js
+++ gamblify-ui/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Welcome To Gamblify</h1>
+      <button>Login</button>
+      <button>Sign-Up</button>
     </div>
   );
 }
